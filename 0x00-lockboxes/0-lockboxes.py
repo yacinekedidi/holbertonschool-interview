373f5713a0c53b5830bb825d4a0c66570686c33b
yacinekedidi <kedidiyacine@gmail.com>
2021-04-21 18:55:46 +0100

    f

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,13 +4,3 @@
 def canUnlockAll(boxes):
     if len(boxes) <= 1:
         return True
-    keys = [0]
-    looped = []
-    while len(keys):
-        for i in boxes[keys[0]]:
-            if i not in keys and i not in looped and i < len(boxes):
-                keys.append(i)
-        looped.append(keys.pop(0))
-        if len(looped) == len(boxes):
-            return True
-    return False
