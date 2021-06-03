c1db6f1316556689b5925379f79301ca5840f591
yacinekedidi <kedidiyacine@gmail.com>
2021-06-03 09:51:01 +0100

    a script that reads stdin line by line and computes metrics

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module
+
+
+
+"""
+import sys
+import signal
+argv = sys.argv
+
+
+def printStatus(status, sum):
+    """
+    print the stats and size.
+    """
+    try:
+        print('File size: {}'.format(sum))
+        [print("{}: {}".format(k, v))
+         for k, v in sorted(status.items()) if v != 0]
+    except Exception:
+        pass
+
+
+status = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
+}
+sum = 0
+list = []
+try:
+    for i, line in enumerate(sys.stdin):
+        list = line.split(' ')
+        if sum and (i+1) % 10 == 1:
+            printStatus(status, sum)
+        try:
+            status[list[-2]] += 1
+        except Exception:
+            pass
+        try:
+            sum += int(list[-1].replace('\n', ''))
+        except Exception:
+            pass
+except KeyboardInterrupt:
+    printStatus(status, sum)
+    raise
