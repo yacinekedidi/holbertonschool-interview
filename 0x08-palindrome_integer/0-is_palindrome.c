6133a19ac47c78352d0b39e636ee0ff7d05cc4f0
yacinekedidi <kedidiyacine@gmail.com>
2021-06-17 20:43:16 +0100

    a function that checks whether or not a given unsigned integer is a palindrome.

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -40,6 +40,11 @@ int is_palindrome(unsigned long n)
 	char *s = itoa(n, 10);
 	size_t i, j;
 
+	if (n < 10 && n >= 0)
+		return (1);
+	if (n < 0)
+		return (0);
+
 	for (i = 0, j = strlen(s) - 1; i < strlen(s); i++, j--)
 	{
 		if (s[i] != s[j])
