596b7d4f77f6d915da406bc2d1e751d10da976aa
yacinekedidi <kedidiyacine@gmail.com>
2021-06-24 21:28:31 +0100

    a method that determines if a given data set represents a valid UTF-8 encoding.

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding.
+    """
+    ones = 0
+    binaryList = ['{:0>8}'.format(str(bin(n))[2:]) for n in data]
+    if binaryList[0][0] == '0':
+        return True
+    else:
+        for i in binaryList[0]:
+            if i == '1':
+                ones += 1
+        if binaryList[0][ones+1] != 0:
+            return False
+        for i in binaryList[1:]:
+            if i[0] != '1' or i[1] != '0':
+                return False
+    return True
