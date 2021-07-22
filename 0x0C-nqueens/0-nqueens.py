889295b7b088f69616193fa6af0a367ef921793e
yacinekedidi <kedidiyacine@gmail.com>
2021-07-22 23:52:53 +0100

    task

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""[summary]"""
+from sys import argv
+argc = len(argv)
+
+
+def nqueens(num, row, res):
+    if row == num:
+        print(res)
+    for col in range(num):
+        if all(col != c and r + c != row + col
+               and r - c != row - col for r, c in res):
+            res.append([row, col])
+            nqueens(num, row + 1, res)
+            res.remove([row, col])
+
+
+if __name__ == "__main__":
+    if argc != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        num = int(argv[1])
+        if num < 4:
+            print("N must be at least 4")
+            exit(1)
+    except Exception:
+        print("N must be a number")
+        exit(1)
+    nqueens(num, 0, [])
