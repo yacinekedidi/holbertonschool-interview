66369930cfa7a1c4c7ac8c8ed716566e4ad544de
yacinekedidi <kedidiyacine@gmail.com>
2021-09-11 16:17:24 +0100

    reddit api

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,50 +1,45 @@
 #!/usr/bin/python3
-"""Module"""
+"""function"""
 import requests
 
 
-def count_words(subreddit='', word_list=[], word_count={}, after=''):
-    """[summary]
-
-    Args:
-        subreddit ([type]): [description]
-        word_list ([type]): [description]
-        word_count (dict, optional): [description]. Defaults to {}.
-        after (str, optional): [description]. Defaults to ''.
+def count_words(subreddit, word_list, words_count={}, after=""):
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    queries the Reddit API, parses the title of all hot articles,
+    prints a sorted count of given keywords
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'user-agent': 'yacinekedidi'}
     params = {'after': after}
     response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200 or not len(word_list):
+    if response.status_code != 200:
+        print()
         return
     r = response.json()
-    data = r.get('data', {})
-    after = data.get("after", '')
-    articles = data.get("children", [])
-    for article in articles:
-        for word in word_list:
-            word = word.lower()
-            how_many =\
-                article.get("data", {}).get("title", '').lower().count(word)
-            if not word_count.get(word):
-                word_count[word] = how_many
-            else:
-                word_count[word] += how_many
+    infos = r.get("data")
+    after = infos.get("after")
+    for c in infos.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for w in word_list:
+            if w.lower() in title:
+                how_many = len([t for t in title if t == w.lower()])
+                if words_count.get(w) is None:
+                    words_count[w] = how_many
+                else:
+                    words_count[w] += how_many
 
     if not after:
-        if not len(word_count.keys()):
+        if len(words_count) == 0:
+            print()
             return
-
-        words = [w for w in sorted(word_count.values())]
+        words = [w for w in sorted(words_count.values())]
         words = words[::-1]
         d = {}
-        for w in words:
-            for k, v in word_count.items():
-                if v == w and v != 0:
-                    d[k] = v
-        word_count = d
-        [print("{}: {}".format(keyword, how_many))
-            for keyword, how_many in word_count.items()]
+        for i in words_count.keys():
+            for j in words:
+                if words_count[i] == j:
+                    d[i] = j
+        words_count = d
+        [print("{}: {}".format(k, v)) for k, v in words_count.items()]
     else:
-        count_words(subreddit, word_list, word_count, after)
+        count_words(subreddit, word_list, words_count, after)
