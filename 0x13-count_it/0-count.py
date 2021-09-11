0ce30ed7f80a0cc747dd4f12f877716136dc8f10
yacinekedidi <kedidiyacine@gmail.com>
2021-09-11 15:31:54 +0100

    reddit api

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import requests
+
+
+def count_words(subreddit, word_list, word_count={}, after=''):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'yacinekedidi'}
+    params = {'after': after}
+    response = requests.get(url, headers=headers, params=params)
+    r = response.json()
+    data = r.get('data')
+    after = r.get("after")
+    articles = data.get("children")
+    for article in articles:
+        for word in word_list:
+            if not word_count.get(word):
+                word_count[word] = 0
+            word_count[word] += \
+                article.get("data").get("title").lower().count(word.lower())
+
+    if not after:
+        if not len(word_count):
+            print()
+            return
+
+        words = [w for w in sorted(word_count.values())]
+        words = words[::-1]
+        d = {}
+        for w in words:
+            for k, v in word_count.items():
+                if v == w:
+                    d[k] = v
+        [print("{}: {}".format(keyword, how_many))
+            for keyword, how_many in d.items()]
+    else:
+        count_words(subreddit, word_list, word_count, after)
