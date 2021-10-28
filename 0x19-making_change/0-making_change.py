266793a899c96fb470de2d877e3627e61a761f9e
yacinekedidi <kedidiyacine@gmail.com>
2021-10-28 19:02:45 +0100

    a

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -26,7 +26,7 @@ def makeChange(coins, total):
     filtered_coins.sort(reverse=True)
 
     for i in filtered_coins:
-        rest = total / i
+        rest = total // i
         total -= (rest * i)
         number_of_coins += rest
         if total == 0:
@@ -34,3 +34,7 @@ def makeChange(coins, total):
     if total != 0:
         return -1
 
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
