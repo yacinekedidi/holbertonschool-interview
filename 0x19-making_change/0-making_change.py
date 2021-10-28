b7eb6f7e65adc1f2deb56e66794fcd66a001ad30
yacinekedidi <kedidiyacine@gmail.com>
2021-10-28 17:59:15 +0100

    a

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def makeChange(coins, total):
+    """
+     determine the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+    if not coins or len(coins) == 0:
+        return -1
+
+    if total in coins:
+        return 1
+
+    number_of_coins = 1
+
+    def is_smaller(num):
+        if num <= total:
+            return True
+        else:
+            return False
+
+    filtered_coins = list(filter(is_smaller, coins))
+    filtered_coins.sort(reverse=True)
+
+    x = filtered_coins[0]
+    for i in filtered_coins:
+        while total - x >= i:
+            x += i
+            number_of_coins += 1
+            if x == total:
+                return number_of_coins
+
+    return -1
