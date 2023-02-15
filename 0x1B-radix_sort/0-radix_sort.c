17df4fba2a36bcbae7ae53a19015dc6a7b397f7e
yacinekedidi <kedidiyacine@gmail.com>
2023-02-15 18:08:21 +0100

    a function that sorts an array of integers in ascending order using the Radix sort algorithm

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,125 @@
+#include "sort.h"
+
+
+/**
+ * get_digit_count - computes the power of a number
+ *
+ * @x: the number
+ *
+ * Return: digit count on a number
+ */
+int get_digit_count(int x)
+{
+	unsigned int count = 0;
+
+	while (x)
+	{
+		x /= 10;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _pow - computes the power of a number
+ *
+ * @x: base value
+ * @y: power value
+ *
+ * Return: the power raised to the base number
+ */
+int _pow(int x, int y)
+{
+	int res = 1;
+
+	while (y--)
+		res *= x;
+	return (res);
+}
+
+/**
+ * get_digit_at - calculates the digit at a given spot
+ *
+ * @n: the number
+ * @spot: the digit spot
+ *
+ * Return: a digit on a specific spot in a given number
+ */
+int get_digit_at(int n, int spot)
+{
+	return (((n / (int) _pow(10, spot)) % 100) % 10);
+}
+
+/**
+ * get_max_digit - calculates the biggest
+ * digit count on an array
+ *
+ * @array: pointer to the address of the first element in an array
+ * @size: the size of the array
+ *
+ * Return: the max digit count on a number in an array
+ */
+int get_max_digit(int *array, size_t size)
+{
+	unsigned int max = 0, count;
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		count = get_digit_count(array[i]);
+		if (count > max)
+			max = count;
+	}
+	return (max);
+}
+
+
+/**
+ * radix_sort - sorts an array of integers in ascending order
+ * using the Radix sort algorithm
+ *
+ * @array: pointer to the address of the first element in an array
+ * @size: the size of the array
+ *
+ * Return: sorted array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int current_digit = 0, max_digit;
+	int *bucket = NULL;
+
+	if (size < 2)
+		return;
+	bucket = malloc(sizeof(int) * size);
+	if (!bucket)
+		return;
+	max_digit = get_max_digit(array, size);
+	int current_digit_value;
+
+	while (max_digit > current_digit)
+	{
+		for (unsigned int i = 0; i < size; i++)
+		{
+			unsigned int j = 0;
+			unsigned int k = 0;
+
+			current_digit_value = get_digit_at(array[i], current_digit);
+			if (i == 0)
+				bucket[i] = array[i];
+			else
+			{
+				for (; j < i; j++)
+					if (current_digit_value < get_digit_at(bucket[j], current_digit))
+						break;
+				if (j != i)
+					for (k = i ; k > j; k--)
+						bucket[k] = bucket[k - 1];
+				bucket[j] = array[i];
+			}
+		}
+		for (unsigned int i = 0;  i < size; i++)
+			array[i] = bucket[i];
+		print_array(array, size);
+		current_digit++;
+	}
+	free(bucket);
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_
+#define _SORT_
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_array(const int *, size_t);
+void radix_sort(int *, size_t);
+int get_digit_count(int);
+int get_digit_at(int, int);
+int get_max_digit(int *, size_t);
+int _pow(int, int);
+
+#endif
